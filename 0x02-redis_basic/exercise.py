96c88769cc6d270f22faab1f95def16b4923a6c9
Thugg55 <100637930+Thugg55@users.noreply.github.com>
2024-07-17 16:29:04 +0100

    Mor on exercise in python

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,29 +2,35 @@
 """
 create a cache class
 store an instance of redis-cli
+define the count_calls decorator
+use the INCR command
 """
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
+import functools
+
+def count_calls(method: Callable) -> Callable:
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = f"{method.__qualname__}:calls"
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 class Cache:
-    
-    
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-
-
-    def get (self,key: str, fn: optional[callable[[bytes]
-        union[str, int, float]]] = None -> union[str, int, float, None]:
+    def get(self, key: str, fn: Optional[Callable[[bytes], Union[str, int, float]]] = None) -> Union[str, bytes, int, float, None]:
         data = self._redis.get(key)
         if data is None:
             return None
@@ -32,10 +38,8 @@ class Cache:
             return fn(data)
         return data
 
-
-    def get_str(self, key:str) -> union[str, None]:
+    def get_str(self, key: str) -> Union[str, None]:
         return self.get(key, lambda data: data.decode('utf-8'))
 
-
-    def get_int(self, key:str) -> union[int, None]:
+    def get_int(self, key: str) -> Union[int, None]:
         return self.get(key, int)
